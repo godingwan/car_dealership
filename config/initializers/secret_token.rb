087e3bde8a830cc615bd1c73febc8771836a4b66
Joshua Lee <jsungholee@gmail.com>
2013-09-23 21:34:45 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarDealer::Application.config.secret_token = 'ccbcdb19f48db1d0cba3bad098352c5c010b9d73c470d39da4ab24efc64d6ce4993dbc7adf1c25191e83d1d3008a7c8d763d55c17a3bf6f1f869c44c8f4f0e75'
